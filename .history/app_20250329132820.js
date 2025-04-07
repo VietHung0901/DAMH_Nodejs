c03ad743d2a2556585316def590361692175f57c
Nguyen Quoc <nguyenvietq306@gmail.com>
2025-04-07 14:20:53 +0700

    complete authorization in header

---
+++ .history/app_20250329132820.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();
+
+app.set("views", __dirname + "/apps/views");
+app.set("view engine", "ejs");
+app.use("/static", express.static(__dirname + "/public"));
+app.use("/assets", express.static(__dirname + "/assets"));
+
+var bodyParser = require('body-parser');
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false})); // false: chỉ hỗ trợ chuỗi hoặc mảng. Nên chuyển thành true nếu cần các object lồng nhau
+app.use(bodyParser.json());
+
+// Các Middleware cần được khai báo trước router được gọi, nếu không express không thể đọc được req.body
+var controller = require(__dirname + "/apps/controllers");
+app.use(controller);
+
+var server = app.listen(3000, function(){
+    console.log("Server is running");
+});
